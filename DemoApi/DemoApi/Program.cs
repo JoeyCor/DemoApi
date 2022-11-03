e7f5352d87bc647214a1b6d36311f552e2435c9c
Joey Cornelissen <Joey.Cornelissen@rabobank.com>
2022-11-03 08:43:29 +0100

    DemoApi solution

---
+++ DemoApi/DemoApi/Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Builder;
+using System.Net;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateTime.Now.AddDays(index),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.MapGet("/hostinfo", () => 
+{
+    var hostName = Dns.GetHostName();
+    var ipAddresses = Dns.GetHostAddresses(hostName).Select(i => i.ToString());
+    return new HostInfo(hostName, string.Join(',', ipAddresses));
+})
+.WithName("GetHostInfo");
+
+app.Run();
+
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+internal record HostInfo(string HostName, string ipAddresses) { }
